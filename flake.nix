9ef10457c3ca55387822a34eac29322ccdf9a148
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-01-10 11:25:03 +0700

    chore: added direnv and flakes

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            SDL2
+            SDL2_ttf
+            SDL2_net
+            SDL2_gfx
+            SDL2_sound
+            SDL2_mixer
+            SDL2_image
+
+            rust-bin.stable.latest.default
+          ];
+        };
+      }
+    );
+}
